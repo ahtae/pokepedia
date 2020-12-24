a22fdd38e95ca4bc50451f685382c85c24fc773d
Kristy Li <kristyli20.0@gmail.com>
2020-12-24 13:05:06 -0500

    chore: Import and export NotFound page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import Pokemons from './pokemons/Pokemons';
 import SinglePokemon from './singlePokemon/SinglePokemon';
+import NotFound from './notFound/NotFound';
 
 export { Pokemons };
 export { SinglePokemon };
+export { NotFound };
