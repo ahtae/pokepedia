42d2111bc9e67a5ee60ff28b65d68c3e29fd9805
Kristy Li <kristyli20.0@gmail.com>
2020-12-24 13:02:17 -0500

    feat: Create a SearchBar component and add styling

---
+++ src/components/searchBar/SearchBar.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
+import './Styles.scss';
+
+const SearchBar = ({
+  handleSearchClick,
+  hasClickedSearch,
+  search,
+  handleSearchChange,
+  possibleWords,
+  handlePossibleWordClick,
+}) => {
+  return (
+    <div className="searchBarContainer">
+      <div className="searchBarContainer__searchBar">
+        <InputBase
+          placeholder="Search"
+          className="searchBarContainer__searchBar__searchInput"
+          onChange={handleSearchChange}
+          value={search}
+          inputProps={{ 'aria-label': 'search' }}
+        />
+        <div className="searchBarContainer__searchBar__button">
+          <button
+            className="searchBarContainer__searchBar__button--removeUnderline"
+            disabled={!search}
+          >
+            <SearchIcon
+              onClick={handleSearchClick}
+              className="searchBarContainer__searchBar__button--removeUnderline"
+            />
+          </button>
+        </div>
+        <div className="searchBarContainer__autocompleteContainer">
+          {!hasClickedSearch &&
+            possibleWords &&
+            possibleWords.map((possibleWord) => (
+              <div
+                key={possibleWord}
+                onClick={() => {
+                  handlePossibleWordClick(possibleWord);
+                }}
+                className="searchBarContainer__autocompleteContainer__word"
+              >
+                {possibleWord}
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
