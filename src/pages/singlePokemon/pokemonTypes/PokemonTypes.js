cf5b662feeab59cca475c27647924ed807ab8d15
Kristy Li <kristyli20.0@gmail.com>
2020-12-25 00:34:16 -0500

    feat: Create a PokemonTypes component

--- src/pages/singlePokemon/pokemonTypes/PokemonTypes.js
+++ src/pages/singlePokemon/pokemonTypes/PokemonTypes.js
@@ -1,14 +1,15 @@
 import { useQuery } from '@apollo/react-hooks';
 import React from 'react';
 import PokemonType from './pokemonType/PokemonType';
+import Row from 'react-bootstrap/Row';
 
 const PokemonTypes = ({ types }) => {
-  const allTypes = types.map((type) => <PokemonType type={type} />);
+  const allTypes = types.map((type) => <PokemonType key={type} type={type} />);
 
   return (
-    <div className="singlePokemonContainer__pokemon__header__badges">
+    <Row className="singlePokemonContainer__pokemon__header__badges">
       {allTypes}
-    </div>
+    </Row>
   );
 };
 
